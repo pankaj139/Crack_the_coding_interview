f8a65d55e75815ec0c8cc095dbfeede42ece9c88
Pankaj Khandelwal <pankaj139@gmail.com>
2023-02-23 12:55:01 +0530

    with hash

--- Chapter 1/Q1-Is_Unique.js
+++ Chapter 1/Q1-Is_Unique.js
@@ -1,3 +1,5 @@
+//Is Unique :  Implement an algorithm to determine if a string has all the unique characters. What if you cannot use additional data structure?
+
 function isUnique(str) {
     let len = str.length;
     for (let index = 0; index < str.length-1; index++) {
@@ -11,6 +13,19 @@ function isUnique(str) {
 
     return true;
 }
+console.assert(isUnique("testing") == false,"return false for testing");
+console.assert(isUnique("tesing"),"return true for testing");
+function isUniqueWithHash(str){
+    let hash = new Map();
+    for (let element of str) {
+        if(hash.has(element)) { return false; }
+        else{
+            hash.set(element,1);
+        }
+        
+    }
+    return true;
+}
+console.assert(isUniqueWithHash("testing") == false,"return false for testing");
+console.assert(isUniqueWithHash("tesing"),"return true for testing");
 
-console.log(isUnique("testing"));
-console.log(isUnique("tesing"));
